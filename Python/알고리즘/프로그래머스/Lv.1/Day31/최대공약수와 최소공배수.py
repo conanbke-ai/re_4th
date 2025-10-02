9e116b58607ae59bb4ef01f542cb6acc3d28d1ca
conanbke-ai <conanbke@gmail.com>
2025-10-03 00:49:18 +0900

    프로그래머스

---
+++ Python/알고리즘/프로그래머스/Lv.1/Day31/최대공약수와 최소공배수.py
@@ -0,0 +1,19 @@
+'''
+두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다. 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+
+* 제한 사항
+    - 두 수는 1이상 1000000이하의 자연수입니다.
+'''
+
+import math
+def solution(n, m):
+    
+    return [math.gcd(n, m), abs(n * m) // math.gcd(n, m)]
+
+'''
+예시1)
+def solution(n, m):
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
+'''
